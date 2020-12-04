f47f1de1fefdb6e9d73f7afd6c896821a32eedcf
lazarmijatovicra942018 <mijatovic.ra94.2018@uns.ac.rs>
2020-12-04 20:19:08 +0100

    #toolbar Implementacija toolbara

---
+++ src/view/MyToolBar.java
@@ -0,0 +1,114 @@
+package view;
+
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
+
+
+
+
+public class MyToolBar extends JToolBar{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MyToolBar()  {
+		// TODO Auto-generated constructor stub
+		
+		
+	
+		super(SwingConstants.HORIZONTAL);
+		//dodaj
+		Icon a = new ImageIcon("images/add_n.png");
+	
+		JButton dodaj = new JButton(a);
+		
+		dodaj.setToolTipText("kreiranje entiteta informacionog sistema ");
+			
+			
+		dodaj.setContentAreaFilled(false);
+		dodaj.setFocusPainted(false);
+		dodaj.setBorderPainted(false);
+		
+		add(dodaj);
+	
+	    addSeparator();
+	    
+	    
+	    //izmeni
+		Icon b = new ImageIcon("images/edit_n.png");
+		
+		JButton izmeni = new JButton(b);
+		
+		izmeni.setToolTipText("otvaranje dijaloga za izmenu označenog entiteta");
+	
+		
+		izmeni.setContentAreaFilled(false);
+		izmeni.setFocusPainted(false);
+		izmeni.setBorderPainted(false);
+		
+		add(izmeni);
+		
+		 addSeparator();
+		    
+		    
+		//obrisi
+		 Icon c = new ImageIcon("images/delete_n.png");
+			
+		JButton obrisi = new JButton(c);
+			
+		obrisi.setToolTipText(" brisanje označenog eniteta iz sistema");
+		
+			
+		obrisi.setContentAreaFilled(false);
+		obrisi.setFocusPainted(false);
+		obrisi.setBorderPainted(false);
+			
+		add(obrisi);
+		
+		java.awt.Color juneau_spring=new java.awt.Color(102, 181, 166, 250);
+		JPanel pan=new JPanel();
+		pan.setBackground(juneau_spring);
+		BoxLayout box=new BoxLayout(pan, BoxLayout.X_AXIS );
+		pan.setLayout(box);
+		pan.add(Box.createGlue());
+        Dimension dim=new Dimension(5000,26);
+	    JTextField txt=new JTextField();
+	    pan.add(txt);
+	    txt.setMaximumSize(dim);
+	    txt.setMinimumSize(dim);
+	    add(pan);
+		
+	  
+		
+		
+		Icon q = new ImageIcon("images/search_n.png");
+		JButton pronadji = new JButton(q);
+		pronadji.setToolTipText("pronadji oznaceni entitet u sistemu");
+		pronadji.setContentAreaFilled(false);
+		pronadji.setFocusPainted(false);
+		pronadji.setBorderPainted(false);
+		add(pronadji);
+		
+		
+		setFloatable(false);
+		
+		setBackground(juneau_spring);
+		
+		
+		
+		}
+	
+
+	
+}
