5c08676f7148fc8254a5f02e59a9d69f0325170f
lazarmijatovicra942018 <mijatovic.ra94.2018@uns.ac.rs>
2020-12-04 20:17:45 +0100

    #glavni_prozor

---
+++ src/studentskaSluzba/MyMain.java
@@ -0,0 +1,14 @@
+package studentskaSluzba;
+
+import view.MainWindow;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainWindow mw = MainWindow.getInstance(); 
+		mw.setVisible(true);
+
+	}
+
+}
---
+++ src/view/MainWindow.java
@@ -0,0 +1,68 @@
+package view;
+
+import java.awt.BorderLayout;
+//import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+//import com.sun.prism.paint.Color;
+//import java.awt.color.*;
+
+import java.awt.Toolkit;
+
+
+
+public class MainWindow extends JFrame{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+	private static MainWindow instance =null;
+
+	private MainWindow() {
+	   initalise();
+	}
+	
+
+
+	private void initalise(){
+		// TODO Auto-generated constructor stub
+	    Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		setTitle("Studentska sluzba");
+		setSize(3*screenWidth /4, 3*screenHeight /4);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setLocationRelativeTo(null);
+		
+		JPanel panel=new JPanel();
+		java.awt.Color banana=new java.awt.Color(255, 225, 53, 183);
+		panel.setBackground(banana);
+		
+	    this.add(panel);
+		
+		
+		MyToolBar t = new MyToolBar();
+		add(BorderLayout.NORTH, t);
+	}
+	
+	 
+	    
+	    public static MainWindow getInstance(){
+	    	if(instance==null){
+	        instance = new MainWindow();
+	    }
+	    	return instance;
+	    }
+
+}
