e13a6ab5d3c782f45c4d2239404029269e7e8c84
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2021-01-01 00:29:49 +0100

    #predmet Implementacija modela predmeta

--- src/model/Predmet.java
+++ src/model/Predmet.java
@@ -1,5 +1,93 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Predmet {
+
+	public enum Semestar {
+		letnji, zimski
+	};
+
+	private String sifra;
+	private String naziv;
+	private Semestar semestar;
+	private int godStudija;
+	private Profesor profesor;
+	private int espb;
+	private ArrayList<Student> suPolozili;
+	private ArrayList<Student> nisuPolozili;
+
+	public Predmet(String sifra, String naziv, Semestar semestar, int godStudija, int espb) {
+		super();
+		this.sifra = sifra;
+		this.naziv = naziv;
+		this.semestar = semestar;
+		this.godStudija = godStudija;
+		this.espb = espb;
+	}
+
+	public String getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(String sifra) {
+		this.sifra = sifra;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public Semestar getSemestar() {
+		return semestar;
+	}
+
+	public void setSemestar(Semestar semestar) {
+		this.semestar = semestar;
+	}
+
+	public int getGodStudija() {
+		return godStudija;
+	}
+
+	public void setGodStudija(int godStudija) {
+		this.godStudija = godStudija;
+	}
+
+	public Profesor getProfesor() {
+		return profesor;
+	}
+
+	public void setProfesor(Profesor profesor) {
+		this.profesor = profesor;
+	}
+
+	public int getEspb() {
+		return espb;
+	}
+
+	public void setEspb(int espb) {
+		this.espb = espb;
+	}
+
+	public ArrayList<Student> getSuPolozili() {
+		return suPolozili;
+	}
+
+	public void setSuPolozili(ArrayList<Student> suPolozili) {
+		this.suPolozili = suPolozili;
+	}
+
+	public ArrayList<Student> getNisuPolozili() {
+		return nisuPolozili;
+	}
+
+	public void setNisuPolozili(ArrayList<Student> nisuPolozili) {
+		this.nisuPolozili = nisuPolozili;
+	}
 
 }
